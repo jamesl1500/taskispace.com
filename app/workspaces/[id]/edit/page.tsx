1150679b24208cb41b916848a4b9f7b0850ad665
James Latten <james.latten@phntm.com>
2025-11-20 12:42:48 -0500

    Fix build errors, will add actual page content soon

---
+++ app/workspaces/[id]/edit/page.tsx
@@ -0,0 +1,18 @@
+/**
+ * Edit Workspace Page
+ * Allows users to edit workspace details.
+ *
+ * @returns {JSX.Element} The edit workspace page component.
+ */
+'use client'
+
+import React from 'react'
+
+export default function EditWorkspacePage() {
+    return (
+        <div>
+            <h1>Edit Workspace</h1>
+            {/* Add form or other UI elements to edit workspace details here */}
+        </div>
+    )
+}
---
+++ app/workspaces/create/page.tsx
@@ -0,0 +1,18 @@
+/**
+ * Create Workspace Page
+ * Provides a form for creating a new workspace.
+ *
+ * @returns {JSX.Element} The create workspace page component.
+ */
+'use client'
+
+import React from 'react'
+
+export default function CreateWorkspacePage() {
+    return (
+        <div>
+            <h1>Create Workspace</h1>
+            {/* Add form or other UI elements to create a new workspace here */}
+        </div>
+    )
+}
